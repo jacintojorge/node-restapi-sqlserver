6e0367f0afef70a13c34b472400b9f338a337ee8
jacintojorge <63351156+jacintojorge@users.noreply.github.com>
2022-06-01 12:00:07 +0200

    conexión con sqlserver

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,1 +1,9 @@
-export const getProducts = (req, res) => res.send('products!!!')
+import { getConnection} from '../database/connection';
+
+export const getProducts = async (req, res) => {
+    const pool = await getConnection();
+    const result = await pool.request().query("SELECT * FROM Products");
+    console.log(result);
+
+    res.json(result.recordset);
+};
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,17 +1,18 @@
-import sql from 'mssql'; 
+import sql from "mssql"; 
 
 const dbSettings = {
-    user: 'nodejs',
-    password: 'ArisFutbol2022.',
-    server: 'cffolympia.database.windows.net', 
-    database: 'olympiadb' ,
+    user: "nodejs",
+    password: "ArisFutbol2022.",
+    server: "cffolympia.database.windows.net", 
+    //server: "10.0.4.180", 
+    database: "olympiadb" ,
     options: {
         encrypt: true,  // for azure
         trustServerCertificate: true,   // change to true for local dev /self-signed certs
     }, 
 };
 
- async function getConnnection() {
+export async function getConnection() {
      try{
         const pool = await sql.connect(dbSettings); 
         return pool;
@@ -21,4 +22,12 @@ const dbSettings = {
     
  }
 
+/*
+// para chequear que la conexión se hace correctamente
+ async function getConnnection() {
+     const pool = await sql.connect(dbSettings);
+     const result = await pool.request().query("SELECT 1");
+     console.log(result);
+ }
  getConnnection();
+ */
