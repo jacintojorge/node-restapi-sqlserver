6a89e9d95d205d1fc71faf83f48470bd603c8cd9
jacintojorge <63351156+jacintojorge@users.noreply.github.com>
2022-06-02 13:18:57 +0200

    final 4

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-import express from 'express';
-import config from './config';
-import productsRoutes from './routes/products.routes';
+import express from "express";
+import config from "./config";
+import productsRoutes from "./routes/products.routes";
+import cors from "cors";
 
 const app = express();
 
 // settings
-app.set('port', config.port);
+app.set("port", config.port);
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({origin:'*'}))
 
 // Routes
 app.use(productsRoutes);
